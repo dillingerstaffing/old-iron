e9ccd47fe28b96ab658fe9071298dd05e75fdfd1
Emi <dillingerstaffing@users.noreply.github.com>
2026-09-13 01:26:43 +0000

    Escape & as &amp; in Google Fonts stylesheet href; version v20260912-oldiron-2125

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // VERSION: bump this string on every deploy that changes cached assets, so clients install a fresh service worker.
-const VERSION = 'v20260912-1725';
+const VERSION = 'v20260912-2125';
 const CACHE = 'old-iron-' + VERSION;
 const OFFLINE_URL = 'index.html';
 
